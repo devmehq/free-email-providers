a999f7cc6c28ab541333de947db35abffaea2858
Spirillen <pdns@protonmail.com>
2024-01-26 23:26:06 +0100

    Tested and found working in bash 5.1.16(1)-release

--- generator.sh
+++ generator.sh
@@ -1,39 +1,45 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+set -ex
 
 GIT_DIR="$(git rev-parse --show-toplevel)"
 
-# shellcheck disable=SC2086
-activeSource="$(grep -vE '^($|#)' $GIT_DIR/pyfunceble/domains/ACTIVE/list)"
-# shellcheck disable=SC2086
-inactiveSource="$(grep -vE '^($|#)' $GIT_DIR/pyfunceble/domains/INACTIVE/list)"
-# shellcheck disable=SC2086
-invalidSource="$(grep -vE '^($|#)' $GIT_DIR/pyfunceble/domains/ACTIVE/list)"
+activeSource="$GIT_DIR/pyfunceble/domains/ACTIVE/list"
+inactiveSource="$GIT_DIR/pyfunceble/domains/INACTIVE/list"
+invalidSource="$GIT_DIR/pyfunceble/domains/INVALID/list"
 
-echo "Writing test results to json files."
+## Active Records
+if [ -r "${activeSource}" ]; then
+    echo "Cleaning up the source files"
+    sed -i "/^#/d;/^$/d" "$activeSource"
+    jq -R -s -c 'split("\n")' <"$activeSource" | jq '.[:-1]' >"$GIT_DIR/active_records.json"
+    echo "Delicious a fresh and updated list of active domains"
 
-if [ -f "$activeSource" ]; then
+else
     echo "Did not find any source file, are you sure @PyFunceble did right?"
     echo "Stopping the process as you encountered an source error"
     exit 1
-elif [ -r "$activeSource" ]; then
-    jq -R -s -c 'split("\n")' <"$activeSource" | jq '.[:-1]' \
-        >"$GIT_DIR/active_records.json"
-    echo "Delicious a fresh and updated list of active domains"
 fi
 
-if [ -f "$inactiveSource" ]; then
+## INactive records
+if [ -r "$inactiveSource" ]; then
+    echo "Cleaning up the source files"
+    sed -i "/^#/d;/^$/d" "$inactiveSource"
+    jq -R -s -c 'split("\n")' <"$inactiveSource" | jq '.[:-1]' >"$GIT_DIR/dead_records.json" &&
+        echo "Perfect, the list of dead records is up to date"
     echo "You should had run PyFunceble earlier..."
-elif [ ! -f "$inactiveSource" ]; then
+
+else
     echo "Perfect: No INACTIVE records found"
-elif [ -r "$inactiveSource" ]; then
-    jq -R -s -c 'split("\n")' <"$inactiveSource" | jq '.[:-1]' \
-        >"$GIT_DIR/dead_records.json"
-    echo "Perfect, the list of dead records is up to date"
 fi
 
-if [ -f "$invalidSource" ]; then
-    echo "Well done, plants of typppooosss thiiiss time"
-elif [ -r "$invalidSource" ]; then
-    jq -R -s -c 'split("\n")' <"$invalidSource" | jq '.[:-1]' \
-        >"$GIT_DIR/dead_records.json"
+## Invalid records
+if [ -r "$invalidSource" ]; then
+    echo "Cleaning up the source files"
+    sed -i "/^#/d;/^$/d" "$invalidSource"
+    jq -R -s -c 'split("\n")' <"$invalidSource" | jq '.[:-1]' >"$GIT_DIR/invalid_records.json" &&
+        echo "Perfect, the list of dead records is up to date"
+else
+    echo "but haed, plants of typppooosss thiiiss time"
+    el
 fi
